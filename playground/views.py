f3ba989b6e58295a7e3beea55ba13f4cb687a3c8
ahita1 <ahitafani833@gmail.com>
2024-03-14 05:31:26 -0700

    debugging my django app haha

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-
+def calculate():
+    x=2
+    y=3
+    return x
 def sayHello(request):
+    x = calculate()
+    y=2
     return render(request , 'hello.html' , {'name' : 'Ahunem Nigussie'})
