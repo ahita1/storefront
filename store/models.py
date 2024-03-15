e6160146806ffaeecdff5d29ec1f6d169ffc2d2e
ahita1 <ahitafani833@gmail.com>
2024-03-15 12:42:54 -0700

    adding an Order models with choices field

--- store/models.py
+++ store/models.py
@@ -9,8 +9,29 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        ('MEMBERSHIP_BRONZE' , 'Bronze'),
+        ('MEMBERSHIP_SILVER' , 'Silver'),
+        ('MEMBERSHIP_GOLD' , 'Gold'),
+        ]
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     email = models.EmailField(unique = True)
     phone_number = models.CharField(max_length = 255)
     birth_date = models.DateField(null = True)
+    membership = models.CharField(max_length = 1 , choices = MEMBERSHIP_CHOICES, default = 'MEMBERSHIP_BRONZE') 
+
+class Order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_CHOICES = [
+        ('PAYMENT_PENDING' , 'Pending'),
+        ('PAYMENT_COMPLETE' , 'Pending'),
+        ('PAYMENT_FAILED' , 'Pending'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add = True)
+    payment_status = models.CharField(max_length = 1 , choices = PAYMENT_CHOICES , default = PAYMENT_PENDING)
