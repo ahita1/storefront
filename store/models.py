e3bf99612289983cae8dd9c3d4852991ca882e3d
ahita1 <ahitafani833@gmail.com>
2024-03-15 13:50:10 -0700

    working with one to many relationships haha

--- store/models.py
+++ store/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length = 255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6 , decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    colection = models.ForeignKey(Collection , on_Delete = models.PROTECT)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -34,10 +39,38 @@ class Order(models.Model):
         ('PAYMENT_FAILED' , 'Pending'),
     ]
     placed_at = models.DateTimeField(auto_now_add = True)
-    payment_status = models.CharField(max_length = 1 , choices = PAYMENT_CHOICES , default = PAYMENT_PENDING)          
+    payment_status = models.CharField(max_length = 1 , choices = PAYMENT_CHOICES , default = PAYMENT_PENDING) 
+    customer = models.ForeignKey(Customer , on_Delete = models.PROTECT )         
 
-
+# One to One relationship between a Customer and Address models haha
 class Address(models.Model):
     street = models.CharField(max_length = 255)
     city = models.CharField(max_length = 255)
     customer = models.OneToOneField(Customer , on_Delete = models.CASCADE , primary_key = True)
+
+
+
+    # ########################################
+# One to Many relationship between a Customer and Address models haha
+# class Address(models.Model):
+#     street = models.CharField(max_length = 255)
+#     city = models.CharField(max_length = 255)
+#     customer = models.ForeignKey(Customer , on_Delete = models.CASCADE)
+    # ########################################
+
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add = 255)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order , on_Delete = models.PROTECT)
+    cart = models.ForeignKey(Cart , on_Delete = models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits = 6 , decimal_places = 2)
+
+class cartItem(models.Model):
+    cart = models.ForeignKey(Cart , on_Delete = models.CASCADE)
+    product = models.ForeignKey(Product , on_Delete = models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
