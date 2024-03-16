93cc7b1b6ced3ad753237c15d08248d28dffda12
ahita1 <ahitafani833@gmail.com>
2024-03-16 05:09:28 -0700

    generic relationships haha

--- tags/models.py
+++ tags/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class Tag(models.Model):
@@ -6,4 +8,6 @@ class Tag(models.Model):
 
 class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag , on_delete = models.CASCADE)
-
+    content_type = models.ForeignKey(ContentType , on_delete = models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
